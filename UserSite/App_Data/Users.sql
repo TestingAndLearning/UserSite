42911def859700a2126531358675872db23a8cfd
mt9304 <mt9304@gmail.com>
2016-10-04 23:53:56 -0700

    Initial commit.

---
+++ UserSite/App_Data/Users.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[Table]
+(
+	[Id] INT NOT NULL PRIMARY KEY IDENTITY, 
+    [Name] NCHAR(10) NOT NULL, 
+    [Username] NCHAR(10) NOT NULL, 
+    [Email] NCHAR(100) NOT NULL, 
+    [DateJoined] DATETIME NOT NULL
+)
---
+++ UserSite/Controllers/HomeController.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using UserSite.Models;
+
+namespace UserSite.Controllers
+{
+
+    public class HomeController : Controller
+    {
+        private UsersDBEntities _db = new UsersDBEntities();
+
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View(_db.Users.ToList());
+        }
+
+        // GET: Home/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Home/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: /Home/Create 
+        [AcceptVerbs(HttpVerbs.Post)]
+        public ActionResult Create([Bind(Exclude = "Id")] User userToCreate)
+        {
+            if (!ModelState.IsValid)
+                return View();
+            _db.Users.Add(userToCreate);
+
+            _db.SaveChanges();
+
+            return RedirectToAction("Index");
+
+        }
+
+        // GET: /Users/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            User user = _db.Users.Find(id);
+            if (user == null)
+            {
+                return HttpNotFound();
+            }
+            return View(user);
+        }
+
+        // POST: /Users/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "ID,Name,Username,Email,DateJoined")] User user)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Entry(user).State = EntityState.Modified;
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(user);
+        }
+
+        // GET: Home/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Home/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
